93d0653f5a62313f0d44f9c2e038b5eadfe2db84
Peter Rong <PeterRong96@gmail.com>
2018-08-10 10:58:38 -0700

    dependency ndncxx and boost added. Now I only need ndncert to work.
    Code is almost ready.
    
    Accidently added some *.so into this repo, deleted
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- app/src/main/jni/ndncert-client.cpp
+++ app/src/main/jni/ndncert-client.cpp
@@ -1,8 +1,12 @@
 //
 // Created by peter on 8/7/18.
 //
-
 #include "com_ndn_jwtan_identitymanager_NdncertClient.h"
+
+// TODO: These includes are just a demo that jni, as included by Alex, works.
+#include <boost/property_tree/info_parser.hpp>
+#include <boost/thread.hpp>
+#include <mutex>
 
 JNIEXPORT jstring JNICALL Java_com_ndn_jwtan_identitymanager_NdncertClient_init
         (JNIEnv * env, jclass obj){
